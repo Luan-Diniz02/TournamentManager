94918af61a9a34b4837734f3e61f41019d762c3d
Luan-Diniz02 <luandiniz852@gmail.com>
2025-09-11 16:46:52 -0300

    Update HistoricoFragment.java
    Pequenas otimizaçoes

--- app/src/main/java/br/com/luandiniz/tournamentmanager/views/fragment/HistoricoFragment.java
+++ app/src/main/java/br/com/luandiniz/tournamentmanager/views/fragment/HistoricoFragment.java
@@ -1,6 +1,5 @@
 package br.com.luandiniz.tournamentmanager.views.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,10 +22,6 @@ import br.com.luandiniz.tournamentmanager.model.Torneio;
 
 public class HistoricoFragment extends Fragment implements HistoricoTorneioAdapter.OnTorneioClickListener {
 
-    private RecyclerView recyclerView;
-    private TextView tvEmptyView;
-    private HistoricoTorneioAdapter adapter;
-    private DAOSQLITE dao;
     private List<Torneio> torneios;
     private List <Duelista> duelistas;
 
@@ -45,14 +39,14 @@ public class HistoricoFragment extends Fragment implements HistoricoTorneioAdapt
 
     private void inicializaView(View view) {
         // Inicializar RecyclerView e TextView
-        recyclerView = view.findViewById(R.id.rv_historico_torneios);
-        tvEmptyView = view.findViewById(R.id.tv_empty_view);
+        RecyclerView recyclerView = view.findViewById(R.id.rv_historico_torneios);
+        TextView tvEmptyView = view.findViewById(R.id.tv_empty_view);
 
         // Configurar RecyclerView
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         // Configurar Adapter
-        adapter = new HistoricoTorneioAdapter(torneios, duelistas, this);
+        HistoricoTorneioAdapter adapter = new HistoricoTorneioAdapter(torneios, duelistas, this);
         adapter.setOnTorneioClickListener(torneio -> {
             TorneioFragment fragment = TorneioFragment.newInstance(torneio.getId());
             requireActivity().getSupportFragmentManager()
@@ -75,7 +69,7 @@ public class HistoricoFragment extends Fragment implements HistoricoTorneioAdapt
 
     private void carregarHistorico() {
         // Obter dados do banco
-        dao = DAOSQLITE.getInstance(requireContext());
+        DAOSQLITE dao = DAOSQLITE.getInstance(requireContext());
         torneios = dao.listarTorneios();
         duelistas = dao.listarDuelistas();
     }
